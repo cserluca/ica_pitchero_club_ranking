e7f1940bb547423240e4efa1c6366798b7b7b577
cserluca <cserluca@DESKTOP-MM0IB1F>
2017-02-20 19:17:16 +0000

    Add project files.

---
+++ ica_pitchero_club_ranking/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ica_pitchero_club_ranking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
